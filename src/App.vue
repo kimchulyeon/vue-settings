1b625c37dc29b4e979d815336504cd7544b81352
kim <kim@kimui-MacBookPro.local>
2022-05-18 14:12:07 +0900

    절대경로 추가

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
-	<div>hello</div>
+	<div>
+		hello
+		<Container />
+	</div>
 </template>
 
 <script>
+import Container from "@/components/Container.vue";
+
 export default {
 	created() {
 		console.log("hi");
 	},
+	components: { Container },
 };
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,18 @@
 const { defineConfig } = require("@vue/cli-service");
+const path = require("path");
+
 module.exports = defineConfig({
 	transpileDependencies: true,
 	devServer: {
 		client: {
 			overlay: false,
+		},
+	},
+	configureWebpack: {
+		resolve: {
+			alias: {
+				"@": path.join(__dirname, "src/"),
+			},
 		},
 	},
 });
