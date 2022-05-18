eecc773c4bb55489aed9ece3d8d22bed6ddb4aad
kim <kim@kimui-MacBookPro.local>
2022-05-18 19:39:25 +0900

    인터셉터 모듈화

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,8 +1,19 @@
+import store from "@/store";
 import axios from "axios";
+import { setInterceptors } from "@/apis/common/interceptors";
 
-const instance = axios.create({
-	baseURL: process.env.VUE_APP_API_URL,
-});
+const createInstance = () => {
+	const instance = axios.create({
+		baseURL: process.env.VUE_APP_API_URL,
+		headers: {
+			Authorization: store.state.token,
+		},
+	});
+
+	return setInterceptors(instance);
+};
+
+const instance = createInstance();
 
 export function registerUser(userdata) {
 	return instance.post("signup", userdata);
