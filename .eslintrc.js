778347b6d3046620e47e34250ed1b75ae9ffd941
kim <kim@kimui-MacBookPro.local>
2022-05-18 13:23:59 +0900

    vue 초기 세팅

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+	root: true,
+	env: {
+		node: true,
+	},
+	extends: ["plugin:vue/essential", "eslint:recommended", "plugin:prettier/recommended"],
+	parserOptions: {
+		parser: "@babel/eslint-parser",
+	},
+	rules: {
+		"no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+		"no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+		"prettier/prettier": [
+			"error",
+			{
+				printWidth: 120,
+				semi: true,
+				useTabs: true,
+				tabWidth: 2,
+				bracketSpacing: true,
+				arrowParens: "avoid",
+				trailingComma: "all",
+				singleQuote: false,
+			},
+		],
+	},
+	overrides: [
+		{
+			files: ["**/__tests__/*.{j,t}s?(x)", "**/tests/unit/**/*.spec.{j,t}s?(x)"],
+			env: {
+				jest: true,
+			},
+		},
+	],
+};
