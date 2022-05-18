30e8e321cd5e248f8d04dfd8a7d0d0e800b18081
kim <kim@kimui-MacBookPro.local>
2022-05-18 19:41:09 +0900

    Vuex token

--- src/store/index.js
+++ src/store/index.js
@@ -7,5 +7,24 @@ export default new Vuex.Store({
 	// state
 	state: {
 		username: "",
+		token: "",
+	},
+	// setState
+	mutations: {
+		setUsername(state, payload) {
+			state.username = payload;
+		},
+		clearUsername(state) {
+			state.username = "";
+		},
+		setToken(state, payload) {
+			state.token = payload;
+		},
+	},
+	// 상태가 변했을 때 감지
+	getters: {
+		isLogin(state) {
+			return state.username !== "";
+		},
 	},
 });
