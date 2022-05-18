eedb69f17caad9b16a262b328e1e9cb49b527e4e
kim <kim@kimui-MacBookPro.local>
2022-05-18 19:40:24 +0900

    컴포넌트

--- src/components/Header/AppHeader.vue
+++ src/components/Header/AppHeader.vue
@@ -1,12 +1,32 @@
 <template>
 	<header>
-		<router-link to="/login">Login</router-link> |
-		<router-link to="/signup">Singup</router-link>
+		<div>
+			<template v-if="isUserLogin">
+				<span>{{ $store.state.username }}</span>
+				<a href="javascript:void(0)" @click="logoutUser">로그아웃</a>
+			</template>
+			<template v-else>
+				<router-link to="/login">Login</router-link> |
+				<router-link to="/signup">Singup</router-link>
+			</template>
+		</div>
 	</header>
 </template>
 
 <script>
-export default {};
+export default {
+	computed: {
+		isUserLogin() {
+			return this.$store.getters.isLogin;
+		},
+	},
+	methods: {
+		logoutUser() {
+			this.$store.commit("clearUsername");
+			this.$router.push("/login");
+		},
+	},
+};
 </script>
 
 <style lang="scss" scoped></style>
